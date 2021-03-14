f3038e784b1aed41064ac8ecbe905e70cddf6c08
Varun <vmadabushi3@gatech.edu>
2021-03-14 18:23:15 -0400

    Initial Commit with multiplication implemented

---
+++ SE2.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+
+class SE2:
+    def __init__(self, *args, **kwargs):
+        if(len(kwargs) == 0):
+            self.__M = np.eye(3)
+        elif(len(kwargs) == 2):
+            R = kwargs['R']
+            x = kwargs['x']
+            if(np.shape(R) != (2,2)):
+                raise ValueError("Rotation Matrix must be orthogonal 2x2 matrix")
+            if(np.shape(x) != (2,1)):
+                raise ValueError("Displacement must be 2x1 vector")
+
+            self.__M = np.hstack((R, x))
+            self.__M = np.vstack((self.__M, np.array([0,0,1])))
+        elif(len(kwargs) == 1):
+            M = kwargs['M']
+            if(np.shape(M) == (3,3) and np.array_equal(M[2,:], np.array([0, 0, 1]))):
+                self.__M = M
+            else:
+                raise ValueError("Not a Homogenous matrix")
+    
+    def getTranslation(self):
+        return self.__M[0:1, 2]
+    
+    def getRotation(self):
+        return self.__M[0:1, 0:1]
+    
+    def getAngle(self):
+        return np.arctan2(M[1,0], M[0,0])
+
+    def __mul__(self, other):
+        m = np.matmul(self.__M, other.__M)
+        return SE2(M=m)
+    
+    def __str__(self):
+        return str(self.__M)
