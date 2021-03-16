69d565e441b3e941e4a0acb37be3a5fd8018a648
Varun <vmadabushi3@gatech.edu>
2021-03-16 11:51:20 -0400

    Changed folder structure to support installation

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+setup(name='Lie',
+      version='1.0',
+      description='Classes implementing Lie Groups and Algebras'
+      author='Varun Madabushi'
+      packages=['Lie'],
+      )
--- tests/testSE2.py
+++ tests/testSE2.py
@@ -1,21 +1,18 @@
 import numpy as np
-import sys
-
-sys.path.insert(1,'../')
 from Lie import SE2
 
 theta = np.pi/2
 R = np.array([[np.cos(theta), -np.sin(theta)], 
                 [np.sin(theta), np.cos(theta)]])
 x = np.array([[1], [0]])
-a = SE2.SE2(R=R, x=x)
+a = SE2(R=R, x=x)
 
 theta = -np.pi/2
 R = np.array([[np.cos(theta), -np.sin(theta)], 
                 [np.sin(theta), np.cos(theta)]])
 x = np.array([[0], [1]])
 
-b = SE2.SE2(R=R, x=x)
+b = SE2(R=R, x=x)
 
 print(a)
 print(b)
