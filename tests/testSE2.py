8a447e088dd9d1b6b3471e6237fbe5b5335f4571
yunzhil <yunzhi.lin@gatech.edu>
2021-07-13 11:46:32 -0400

    add support for 2D matrix input

--- tests/testSE2.py
+++ tests/testSE2.py
@@ -1,10 +1,29 @@
-from Lie.group.SE3 import Homog
+#!/usr/bin/python3
+#=========================== testSE2 ===========================
+#
+# @brief    Code to test basic multiplication functionality of SE2
+#
+#=========================== testSE2 ===========================
+
+#
+# @file     testSE2.py
+#
+# @author   Yunzhi Lin, yunzhi.lin@gatech.edu
+# @date     2021/07/13 [modified]
+#
+# @quit
+#=========================== testSE2 ===========================
+
+#==[0] Prep environment
+#
 import numpy as np
 import Lie.group.SE2.Homog
 
-theta = np.pi/3
+#==[1] Test Homog objects multiplication
+#
+theta = 0
 R = Lie.group.SE2.Homog.rotationMatrix(theta)
-x = np.array([[1], [0]])
+x = np.array([[200], [200]])
 a = Lie.group.SE2.Homog(R=R, x=x)
 
 theta = -np.pi/2
@@ -13,10 +32,51 @@ x = np.array([[0], [1]])
 
 b = Lie.group.SE2.Homog(R=R, x=x)
 
-c = np.array([0,1])
+print('Test Homog objects multiplication')
+print('Homog object:\n', a)
+print('Homog object:\n', b)
+print('Result:\n', a*b)
+print('\n')
 
-print(a)
-print(b)
-print(a*b)
-print(a*c)
-print(a.inv() * a)
+#==[2] Test Homog object multiplication with point
+#
+b = np.array([-12, 18])
+
+print('Test Homog object multiplication with point')
+print('Homog object:\n', a)
+print('Point:\n', b)
+print('Result:\n', a*b)
+print('\n')
+
+#==[3] Test Homog object multiplication with velocity
+#
+b = np.array([-12,18,1])
+
+print('Test Homog object multiplication with velocity')
+print('Homog object:\n', a)
+print('Velocity:\n', b)
+print('Result:\n', a*b)
+print('\n')
+
+#==[4] Test Homog object multiplication with 2D matrix of points
+#
+b = np.array([[-12,18],[18,18]])
+
+print('Test Homog object multiplication with 2D matrix of points')
+print('Homog object:\n', a)
+print('2D matrix of points:\n', b)
+print('Result:\n', a*b)
+print('\n')
+
+#==[5] Test Homog object multiplication with 2D matrix of velocities
+#
+b = np.array([[-12,18],[18,18],[1,1]])
+
+print('Test Homog object multiplication with 2D matrix of velocities')
+print('Homog object:\n', a)
+print('2D matrix of velocities:\n', b)
+print('Result:\n', a*b)
+print('\n')
+
+#
+#=========================== testSE2 ============================
