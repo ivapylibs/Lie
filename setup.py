f84c27b00c26ff41e204c169c36b367c5a6f2318
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2023-02-17 16:04:27 -0500

    Use find_packages in setup

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
-from setuptools import setup
-setup(name='Lie',
-      version='1.0',
-      description='Classes implementing Lie Groups and Algebras',
-      author='Varun Madabushi',
-      packages=['Lie'],
-      install_requires=['numpy', 'scipy', 'matplotlib']
-      )
+from setuptools import setup, find_packages
+
+setup(
+    name="Lie",
+    version="1.0.1",
+    description="Classes implementing Lie Groups and Algebras",
+    author="Varun Madabushi",
+    packages=find_packages(),
+    install_requires=["numpy", "scipy", "matplotlib"],
+)
